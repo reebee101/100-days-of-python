43a96510a1c1248f4881b4da714178466aeb725e
reebee101 <141925913+reebee101@users.noreply.github.com>
2023-08-11 11:02:52 +0300

    Add files via upload

---
+++ a3.py
@@ -0,0 +1,191 @@
+"""A board is a list of list of str. For example, the board
+    ANTT
+    XSOB
+is represented as the list
+    [['A', 'N', 'T', 'T'], ['X', 'S', 'O', 'B']]
+
+A word list is a list of str. For example, the list of words
+    ANT
+    BOX
+    SOB
+    TO
+is represented as the list
+    ['ANT', 'BOX', 'SOB', 'TO']
+"""
+
+
+def is_valid_word(wordlist, word):
+    """ (list of str, str) -> bool
+
+    Return True if and only if word is an element of wordlist.
+
+    >>> is_valid_word(['ANT', 'BOX', 'SOB', 'TO'], 'TO')
+    True
+    """
+    return word in wordlist
+
+def make_str_from_row(board, row_index):
+    """ (list of list of str, int) -> str
+
+    Return the characters from the row of the board with index row_index
+    as a single string.
+
+    >>> make_str_from_row([['A', 'N', 'T', 'T'], ['X', 'S', 'O', 'B']], 0)
+    'ANTT'
+    """
+    str_from_row ='' #empty variable
+    for ch in board[row_index]: #for loop for characters in board row
+        str_from_row+=ch #adding characters in the empty variable
+    return str_from_row
+
+    
+def make_str_from_column(board, column_index):
+    """ (list of list of str, int) -> str
+
+    Return the characters from the column of the board with index column_index
+    as a single string.
+
+    >>> make_str_from_column([['A', 'N', 'T', 'T'], ['X', 'S', 'O', 'B']], 1)
+    'NS'
+    """
+    str_from_column='' #empty variable
+    for row in board: #for loop for each row in board
+        str_from_column+=row[column_index] #row[1] second character in row
+    return str_from_column
+
+
+def board_contains_word_in_row(board, word):
+    """ (list of list of str, str) -> bool
+
+    Return True if and only if one or more of the rows of the board contains
+    word.
+
+    Precondition: board has at least one row and one column, and word is a
+    valid word.
+
+    >>> board_contains_word_in_row([['A', 'N', 'T', 'T'], ['X', 'S', 'O', 'B']], 'SOB')
+    True
+    """
+
+    for row_index in range(len(board)): #loop index in length of board
+        if word in make_str_from_row(board, row_index):#checks if word exists in board in row
+            return True
+
+    return False
+
+
+def board_contains_word_in_column(board, word):
+    """ (list of list of str, str) -> bool
+
+    Return True if and only if one or more of the columns of the board
+    contains word.
+
+    Precondition: board has at least one row and one column, and word is a
+    valid word.
+
+    >>> board_contains_word_in_column([['A', 'N', 'T', 'T'], ['X', 'S', 'O', 'B']], 'NO')
+    False
+    """
+    for row_index in range(len(board)):
+        for column_index in range(len(board[row_index])):
+            if word in make_str_from_column(board,column_index):
+                return True
+    return  False
+    
+
+def board_contains_word(board, word):
+    """ (list of list of str, str) -> bool
+
+    Return True if and only if word appears in board.
+
+    Precondition: board has at least one row and one column.
+
+    >>> board_contains_word([['A', 'N', 'T', 'T'], ['X', 'S', 'O', 'B']], 'ANT')
+    True
+    """
+    return board_contains_word_in_row(board, word) or  board_contains_word_in_column(board, word)
+
+
+
+def word_score(word):
+    """ (str) -> int
+
+    Return the point value the word earns.
+
+    Word length: < 3: 0 points
+                 3-6: 1 point per character for all characters in word
+                 7-9: 2 points per character for all characters in word
+                 10+: 3 points per character for all characters in word
+
+    >>> word_score('DRUDGERY')
+    16
+    """
+    word_score=0
+    word_len = len(word)
+    if word_len <3 :
+        word_score=0
+    if word_len >=3 and word_len <=6:
+        word_score = word_len
+    if word_len >=7 and word_len <=9:
+        word_score = word_len *2
+    if word_len >= 10:
+        word_score = word_len *3
+    return word_score
+    
+
+def update_score(player_info, word):
+    """ ([str, int] list, str) -> NoneType
+
+    player_info is a list with the player's name and score. Update player_info
+    by adding the point value word earns to the player's score.
+
+    >>> update_score(['Jonathan', 4], 'ANT')
+    """
+    player_info[0]= player_info[0]+str(word_score(word))
+
+def num_words_on_board(board, words):
+    """ (list of list of str, list of str) -> int
+
+    Return how many words appear on board.
+
+    >>> num_words_on_board([['A', 'N', 'T', 'T'], ['X', 'S', 'O', 'B']], ['ANT', 'BOX', 'SOB', 'TO'])
+    3
+    """
+    num_words = 0 
+    for word in words:
+        if board_contains_word(board,words):
+            num_words+=1
+    return num_words
+
+
+def read_words(words_file):
+    """ (file open for reading) -> list of str
+
+    Return a list of all words (with newlines removed) from open file
+    words_file.
+
+    Precondition: Each line of the file contains a word in uppercase characters
+    from the standard English alphabet.
+    """
+    word_list = []
+    
+    for line in words_file:
+        word_list.append(line.rstrip('\n'))
+        
+    return word_list    
+
+
+def read_board(board_file):
+    """ (file open for reading) -> list of list of str
+
+    Return a board read from open file board_file. The board file will contain
+    one row of the board per line. Newlines are not included in the board.
+    """
+    board_list=[]
+    for line in board_file:
+        char_list=[]
+        for ch in line.rstrip('\n'):
+            char_list.append(ch)
+        board_list.append(char_list)
+    return board_list
+   
